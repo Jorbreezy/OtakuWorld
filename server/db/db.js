2157904a4f8ac49208f8a486d044df9a69422075
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-07-02 17:03:06 -0400

    Added dotenv, hid DB_URI

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,13 @@
 const { Pool } = require('pg');
-const PG_URI = 'postgres://ydtpmvfk:TSs75C8j4ZrnM6opWzYbIB9KkLNiZm4Z@ruby.db.elephantsql.com:5432/ydtpmvfk';
 
-const pool  = new Pool({
-    connectionString: PG_URI
-})
+const PG_URI = process.env.DB_URI;
+
+const pool = new Pool({
+  connectionString: PG_URI,
+});
 
 module.exports = {
-    query: (text, params, callback) => {
-        return pool.query(text, params, callback);
-    }
-}
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 const PORT = 3000;
 
-//Serve Static file
-app.use("/dist", express.static(path.join(__dirname, "../dist")));
+// Serve Static file
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
-//Parse Incoming body requests
+// Parse Incoming body requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Route handlers
+// Route handlers
 
-//Main Get Request, Send html file
-app.get("/", (req, res) => {
-  return res.sendFile(path.resolve(__dirname, '../client/index.html'));
-});
+// Main Get Request, Send html file
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
 
-//Catch all error handler
-app.all("*", (req, res) => {
-  return res.status(404).send("Page not found");
-});
+// Catch all error handler
+app.all('*', (req, res) => res.status(404).send('Page not found'));
 
-//Global Error Handler
-app.use((err, req, res) => {
-  return res.status(400).send(err);
-});
+// Global Error Handler
+app.use((err, req, res) => res.status(400).send(err));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
